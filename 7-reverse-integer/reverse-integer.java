7ca40fb165454930b28ef65f8d9a9567fc3c4292
Padmapriyarajasekaran <124979229+Padmapriyarajasekaran@users.noreply.github.com>
2025-08-27 20:24:34 +0530

    Time: 1 ms (81.56%) | Memory: 40.7 MB (85.75%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,11 +3,7 @@ class Solution {
         int sum =0;
         while(x!=0){
             int a = x%10;
-            // Check for overflow before actually multiplying
-            if (sum > Integer.MAX_VALUE / 10 || (sum == Integer.MAX_VALUE / 10 && a > 7)) {
-                return 0;
-            }
-            if (sum < Integer.MIN_VALUE / 10 || (sum == Integer.MIN_VALUE / 10 && a < -8)) {
+            if(sum > Integer.MAX_VALUE/10 || sum < Integer.MIN_VALUE/10){
                 return 0;
             }
             sum = sum *10 +a;
