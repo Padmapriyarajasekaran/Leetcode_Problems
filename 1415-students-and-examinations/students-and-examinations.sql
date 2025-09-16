658a70065d415700dcf33de1bb9a4168c35bcc3d
Padmapriyarajasekaran <124979229+Padmapriyarajasekaran@users.noreply.github.com>
2025-09-16 20:47:57 +0530

    Time: 1413 ms (7.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,6 @@
+select Students.student_id, student_name, Subjects.subject_name,count(Examinations.subject_name) as attended_exams
+from Students
+join Subjects
+left join Examinations on Students.student_id = Examinations.student_id and Subjects.subject_name = Examinations.subject_name
+group by 1,2,3
+order by 1,3
