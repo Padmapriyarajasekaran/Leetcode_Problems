1c6f4be2af7f5bfe1ceafe36afe60675016ae903
Padmapriyarajasekaran <124979229+Padmapriyarajasekaran@users.noreply.github.com>
2025-09-18 20:30:22 +0530

    Time: 1479 ms (6.58%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,4 +1,4 @@
-select Students.student_id, student_name, Subjects.subject_name,count(Examinations.subject_name) as attended_exams
+select Students.student_id, Student_name,Subjects.subject_name,count(Examinations.subject_name) as attended_exams
 from Students
 join Subjects
 left join Examinations on Students.student_id = Examinations.student_id and Subjects.subject_name = Examinations.subject_name
